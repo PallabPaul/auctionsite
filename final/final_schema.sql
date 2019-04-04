181f00879a1fde3d7448e572cc85d63029541f6f
PallabPaul <pallab.paul000@gmail.com>
2019-04-04 18:28:10 -0400

    upd

--- final/final_schema.sql
+++ final/final_schema.sql
@@ -10,13 +10,37 @@ CREATE TABLE `users` (
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+LOCK TABLES `users` WRITE;
+INSERT INTO `users` VALUES ('admin','password','yes');
+INSERT INTO `users` VALUES ('test','test','no');
+UNLOCK TABLES;
+
 DROP TABLE IF EXISTS `items`;
 
 CREATE TABLE `items` (
+  `itemid` int,
   `username` varchar(50),
   `category` varchar(50) NOT NULL,
   `brand` varchar(50),
   `year` varchar(50),
   `type` varchar(50),
-  PRIMARY KEY (`username`,`category`,`type`)
+  PRIMARY KEY (`itemid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `auctions`;
+
+CREATE TABLE `auctions` (
+  `username` varchar(50),
+  `itemid` int,
+  
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `bids`;
+
+CREATE TABLE `bids` (
+  `username` varchar(50),
+  `itemid` int,
+
+
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
